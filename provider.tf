d05b410650f9e4dc2e728b8f4a1f881ea0d4816b
Pietrokechi <pkech@yahoo.com>
2021-01-15 12:50:13 +0000

    New Commit with incorrect cred

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAU5JLJ5VI3CREQGO6"
-  secret_key = "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"
+  access_key = "${file("../access.txt")}"
+  secret_key = "${file("../secret.txt")}"
 }
 
